18960516f05a04bb3526b1c8faa63ad7deeb377b
nirvanaGarciaV <84489764+nirvagarcia@users.noreply.github.com>
2024-03-17 22:35:45 -0500

    using makeStyles to reduce css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,52 @@
 import React from "react";
 import "../styles/Home.css";
+import { makeStyles } from "@mui/material";
+
+const useStyles = makeStyles((theme) => ({
+	home: {
+		height: '100vh',
+		display: 'flex',
+		alignItems: "center",
+		justifyContent: "center",
+		textAlign: "center",
+		cursor: 'default',
+	},
+	homeContainer: {
+		maxWidth: "800px",
+		margin: "0 auto",
+		padding: "20px"
+	},
+}));
 
 const Home = () => {
-return (   	
-	<div id="home">	
-		<div className="home-container">
-			
-			<div style={{ marginTop: 25, marginBottom: 25 }} className="home-title">
-				Disfruta de
-				<img 
-					src="assets/RedAkiraLetters.png" 
-					alt="Akira" 
-					style={{ height: '46px', marginLeft: 16, paddingTop: 6}} 
-				/>
-			</div>
+	const classes = useStyles();
 
-			<div className="home-avatar" style={{ marginTop: 35, marginBottom: 35 }} >
-				<img src="assets/AkiraPhonesBanner.png" alt="Avatar" />
-			</div>							 
-			
-			<div className="home-desc">
-				Transforma tu experiencia al comprar merch asiático desde PE
-            	<img style={{marginRight: 12}} src="/assets/peru.png" alt="Peru" className="peru-image"/>
-				Encontrarás productos de K-pop, animes y mangas cautivadores ¡Solo en Akira Mobile!
-          	</div>
-			
+	return (   	
+		<div className={classes.home}>	
+			<div className={classes.homeContainer}>
+				
+				<div style={{ marginTop: 25, marginBottom: 25 }} className="home-title">
+					Disfruta de
+					<img 
+						src="assets/RedAkiraLetters.png" 
+						alt="Akira" 
+						style={{ height: '46px', marginLeft: 16, paddingTop: 6}} 
+					/>
+				</div>
+
+				<div className="home-avatar" style={{ marginTop: 35, marginBottom: 35 }} >
+					<img src="assets/AkiraPhonesBanner.png" alt="Avatar" />
+				</div>							 
+				
+				<div className="home-desc">
+					Transforma tu experiencia al comprar merch asiático desde PE
+					<img style={{marginRight: 12}} src="/assets/peru.png" alt="Peru" className="peru-image"/>
+					Encontrarás productos de K-pop, animes y mangas cautivadores ¡Solo en Akira Mobile!
+				</div>
+				
+			</div>
 		</div>
-	</div>
-);
+	);
 };
 
 export default Home;
